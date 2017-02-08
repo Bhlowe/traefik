3181a789a684f9ac925bd9c382f0d8b8aeed8b07
bhlowe <bhlowe@gmail.com>
2017-02-07 19:17:56 -0800

    test

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-docker run traefik-dev:master ls -alF static/app/sections
-docker run containous/traefik ls -alF static/app/sections
+# docker run traefik-dev:master ls -alF static/app/sections
+# docker run containous/traefik ls -alF static/app/sections
 
-exit
+# exit
 
 docker run -d -p 8080:8080 -p 443:443 -p 80:80 \
 -v /opt/traefik/traefik.toml:/etc/traefik/traefik.toml \
