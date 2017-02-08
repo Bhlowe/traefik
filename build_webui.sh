14ebf556f1fee99e14ceea974e4f8936c67cd84f
Eugene Ustimenko <evgenyustimenko@gmail.com>
2017-02-08 10:29:26 +0700

    Refactoring

--- build_webui.sh
+++ build_webui.sh
@@ -1,7 +1,3 @@
 rm -rf static
 
-make build           # Generate Docker image
-# make generate-webui  # Generate static contents in `traefik/static/` folder.
-
-
-# make binary image
+make binary image
